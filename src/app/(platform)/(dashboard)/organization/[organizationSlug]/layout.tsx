4f7d75549c1f8ddc6e060526c2a37b3d74c5bdba
Cristian Davila <cristiand395@gmail.com>
2025-01-31 21:00:28 +0100

    Remove unnecessary folder and fix layout typo

--- src/app/(platform)/(dashboard)/organization/[organizationSlug]/layout.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationSlug]/layout.tsx
@@ -1,6 +1,6 @@
 import OrgControl from "./_components/org-control";
 
-export default function OrganizationIdLayout({
+export default function OrganizationSlugLayout({
   children
 } : {
   children: React.ReactNode;
